4fcfb3a2b91051131d7c99cb122c298f9cd64b3a
Howie lynch <THEGRINCH1985@GMAIL.COM>
2019-02-07 16:46:20 +0000

    files

---
+++ Lab_3_Maze_Runner/script.js
@@ -0,0 +1,73 @@
+const ctx = myCanvas.getContext("2d");
+
+const WALL_COLOUR = 'blue';
+const MAZE_COLS = 20;
+const MAZE_ROWS = 20;
+const WALL_WIDTH = 20;
+const WALL_HEIGHT = 20;
+const MAZE_BLOCK_GAP = 2;
+
+
+
+let mazeGrid = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, , 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1,
+    1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1,
+    1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+    1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1,
+    1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1,
+    1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+
+
+
+];
+
+function drawMaze() {
+    ctx.fillStyle = WALL_COLOUR;
+
+    for (let r = 0; r < MAZE_ROWS; r++) {
+        for (let c = 0; c < MAZE_COLS; c++) {
+
+            let gridIndex = c + (MAZE_COLS * r);
+
+            if (mazeGrid[gridIndex] == 1) {
+
+                let reCTX = WALL_WIDTH * c;
+                let rectY = WALL_HEIGHT * r;
+
+                let recW = WALL_WIDTH - MAZE_BLOCK_GAP;
+                let recH = WALL_HEIGHT - MAZE_BLOCK_GAP;
+
+                ctx.fillRect(reCTX, rectY, recH, recW);
+            }
+
+        }
+
+    }
+
+
+}
+
+function gameLoop() {
+
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    drawMaze();
+
+    requestAnimationFrame(gameLoop);
+
+
+}
+gameLoop();
