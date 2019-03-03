24b31fce57913c9a8bc28c2ee42e688dee106c96
Howie lynch <THEGRINCH1985@GMAIL.COM>
2019-03-03 20:41:31 +0000

    Added Bullets

--- CA/www/script.js
+++ CA/www/script.js
@@ -1,11 +1,8 @@
-// create a new scene named "Game"
-//let gameScene = new Phaser.Scene('Game');
 
-// our game's configuration
 var config = {
   type: Phaser.AUTO,
-  width: 290,
-  height: 350,
+  width: 1200,
+  height: 950,
   physics: {
     default: 'arcade',
     arcade: {
@@ -28,11 +25,15 @@ let donk;
 
 // some parameters for our scene (our own customer variables - these are NOT part of the Phaser API)
 let marioSpeed = 1.5;
-let enemyMaxY = 280;
-let enemyMinY = 80;
+//let enemyMaxY = 280;
+//let enemyMinY = 80;
 let gameOver = false;
 var map;
+var path;
 
+var enemies;
+
+var ENEMY_SPEED = 1/10000;
 // create the game, and pass it the configuration
 let game = new Phaser.Game(config);
 
@@ -43,8 +44,8 @@ let game = new Phaser.Game(config);
 
   function preload() {
 
-  this.load.image("tiles", 'assets/Level1-1StageBroken.png')
-  this.load.tilemapTiledJSON('map', 'assets/DK8BIT.json');
+  this.load.image("tiles", 'assets/BigDonk.jpg')
+  this.load.tilemapTiledJSON('map', 'assets/BigDonk.json');
  
 
 
@@ -112,52 +113,87 @@ let game = new Phaser.Game(config);
   //this.load.image('treasure', 'assets/treasure.png');
 };
 
+
+
 var Enemy = new Phaser.Class({
- 
+
   Extends: Phaser.GameObjects.Image,
 
   initialize:
 
   function Enemy (scene)
   {
-      Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'enemy');
+      Phaser.GameObjects.Image.call(this, scene, 0, 0, 'B5');
 
+      this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
+     // this.hp = 0;
+  },
+
+  startOnPath: function ()
+  {
+      this.follower.t = 0;
+      this.hp = 100;
+      
+      path.getPoint(this.follower.t, this.follower.vec);
+      
+      this.setPosition(this.follower.vec.x, this.follower.vec.y);            
+  },
+  receiveDamage: function(damage) {
+      this.hp -= damage;           
+      
+      // if hp drops below 0 we deactivate this enemy
+      if(this.hp <= 0) {
+          this.setActive(false);
+          this.setVisible(false);      
+      }
   },
   update: function (time, delta)
   {
-                // move the t point along the path, 0 is the start and 0 is the end
-                this.follower.t += ENEMY_SPEED * delta;
-            
-                // get the new x and y coordinates in vec
-                path.getPoint(this.follower.t, this.follower.vec);
-                
-                // update enemy x and y to the newly obtained x and y
-                this.setPosition(this.follower.vec.x, this.follower.vec.y);
-     
-                // if we have reached the end of the path, remove the enemy
-                if (this.follower.t >= 1)
-                {
-                    this.setActive(false);
-                    this.setVisible(false);
+      this.follower.t += ENEMY_SPEED * delta;
+      path.getPoint(this.follower.t, this.follower.vec);
+      
+      this.setPosition(this.follower.vec.x, this.follower.vec.y);
+
+      if (this.follower.t >= 1)
+      {
+          this.setActive(false);
+          this.setVisible(false);
+      }
   }
 
 });
+
+function getEnemy(x, y, distance) {
+  var enemyUnits = enemies.getChildren();
+  for(var i = 0; i < enemyUnits.length; i++) {       
+      if(enemyUnits[i].active && Phaser.Math.Distance.Between(x, y, enemyUnits[i].x, enemyUnits[i].y) < distance)
+          return enemyUnits[i];
+  }
+  return false;
+} 
+
+
+
+
 // executed once, after assets were loaded
 function create() {
   
   //map = this.add.tilemap('map');
     // When loading a CSV map, make sure to specify the tileWidth and tileHeight!
-    const map = this.make.tilemap({ key: "map", tileWidth: 8, tileHeight: 8 });
-    const tileset = map.addTilesetImage("DK8BIT","tiles");
+    const map = this.make.tilemap({ key: "map", tileWidth: 32, tileHeight: 32});
+    const tileset = map.addTilesetImage("BigDonk","tiles");
+    //const tileset = map.addTilesetImage("BigDonk","Platforms");
     const layer = map.createDynamicLayer(0, tileset); // layer index, tileset, x, y
+    const Platforms = map.createDynamicLayer('Platforms', tileset, 0,0);
+    const Ladders = map.createDynamicLayer('Ladders', tileset, 0,0);
   //map = this.make.tilemap({key: 'map'});
-
+  Platforms.setCollisionByExclusion([-1]);
   this.data.set('lives', 3);
   this.data.set('level', 1);
   this.data.set('High Score', 2000);
   //layer.setCollisionBetween('mario');
   layer.setCollisionByProperty({ collides: true });
-  layer.setScale(1.25, 1.25);
+  //layer.setScale(1.25, 1.25);
   //layer.setCollisionBetween('mario');
   var text = this.add.text(200, 15, '', { font: '12px Courier', fill: '#00ff00' });
 
@@ -168,44 +204,19 @@ function create() {
   ]);
   platforms = this.physics.add.staticGroup();
   // background
-  let bg = this.add.sprite(200, 400, 'black');
-  //Rails
-  //Bottom to Top
-
-
-  //Ladders 1 
-  // let Ladder1 = this.add.sprite(115, 85, 'BigLadder')
-  // let Ladder2 = this.add.sprite(115, 70, 'SmallLadder');
-  // let Ladder22 = this.add.sprite(115, 50, 'SmallLadder');
-  // //Ladders 2
-  // let Ladder3 = this.add.sprite(95, 85, 'BigLadder')
-  // let Ladder4 = this.add.sprite(95, 70, 'SmallLadder');
-  // let Ladder23 = this.add.sprite(95, 50, 'SmallLadder');
-  // //Ladder 3 
-  // let Ladder5 = this.add.sprite(135, 85, 'SmallLadder');
-
-  // //Bottom Level Ladders
-  // let Ladder6 = this.add.sprite(115, 380, 'SmallLadder');
-
-  // let Ladder11 = this.add.sprite(195, 340, 'BigLadder')
-  // let Ladder12 = this.add.sprite(195, 360, 'SmallLadder');
-  // let Ladder13 = this.add.sprite(195, 380, 'SmallLadder');
+  //let bg = this.add.sprite(200, 400, 'black');
  
-
-  // ladder1234 =this.physics.add.sprite(135,340,'BigLadder');
-  // ladder1234.setBounce(0.2);
-  // ladder1234.setCollideWorldBounds(true);
-  // this.physics.add.collider(ladder1234, platforms);
+ 
 
 
   //bARRELS
 
   
-  Barrelling =this.physics.add.sprite(100,55,'B5');
-  Barrelling.setBounce(0.2);
-  Barrelling.setCollideWorldBounds(true);
-  this.physics.add.collider(  Barrelling, platforms);
-
+  //Barrelling =this.physics.add.sprite(400,55,'B5');
+  //Barrelling.setBounce(0.2);
+  //Barrelling.setCollideWorldBounds(true);
+  //this.physics.add.collider(  Barrelling, Platforms);
+/*
   this.anims.create({
     key: 'play4',
     frames: [
@@ -218,33 +229,47 @@ function create() {
     repeat: -1
   });
   Barrelling.play('play4');
-
+*/
       // this graphics element is only for visualization, 
     // its not related to our path
+
+    //Enemies
     var graphics = this.add.graphics();    
+    //drawLines(graphics);
+    path = this.add.path(855, 495);
+    path.lineTo(255, 495);
+    path.lineTo(255, 645);
+    path.lineTo(585, 645);
+    path.lineTo(585, 735);
+    path.lineTo(405, 735);
+    path.lineTo(405, 835);
+    path.lineTo(605, 835);
+    path.lineTo(605, 925);
+    path.lineTo(105, 925);
+    //path.lineTo(480, 544);
+   
     
-    // the path for our enemies
-    // parameters are the start x and y of our path
-    path = this.add.path(96, -32);
-    path.lineTo(96, 164);
-    path.lineTo(480, 164);
-    path.lineTo(480, 544);
+    enemies = this.physics.add.group({ classType: Enemy, runChildUpdate: true });
     
-    graphics.lineStyle(3, 0xffffff, 1);
-    // visualize the path
-    path.draw(graphics);
- 
+   
+
+    this.nextEnemy = 0;
+    
+    this.physics.add.overlap(enemies);
+    
+  
 
 
   //Mario
-  mario = this.physics.add.sprite(45, 305, 'mario', 0);
+  mario = this.physics.add.sprite(225, 305, 'mario', 0);
   mario.setBounce(0.2);
   mario.setCollideWorldBounds(true);
+  mario.setScale(2, 2);
   this.anims.create({
     key: 'left',
     frames: this.anims.generateFrameNumbers('mario', {
       start: 8,
-      end: 13
+      end: 12
     }),
     frameRate: 10,
     repeat: -1
@@ -266,17 +291,27 @@ function create() {
       frame: 0
     }],
     frameRate: 20
+  }); this.anims.create({
+    key: 'hit',
+    frames: this.anims.generateFrameNumbers('mario', {
+      start: 7,
+      end: 7
+    }),
+    frameRate: 10,
+    repeat: -1
   });
 
 
 
 //Donkey Kong Images 
 
-  DK = this.physics.add.sprite(50, 55, 'DK1');
+  DK = this.physics.add.sprite(855, 55, 'DK1');
   DK.setBounce(.5);
   DK.setCollideWorldBounds(true);
-  this.physics.add.collider(DK, layer);
-  //layer.setCollisionBetween(19, 44);
+  this.physics.add.collider(DK, Platforms);
+
+  DK.setScale(2, 2);
+
   this.anims.create({
     key: 'play1',
     frames: [
@@ -304,11 +339,12 @@ DK.play('play1')
         frameRate: 1,
         repeat: -1
     });
-    Princess = this.physics.add.sprite(150, 55, 'Princess',0);
-    Princess.setBounce(0.01);
+    Princess = this.physics.add.sprite(750, 75, 'Princess',0);
+
     Princess.setCollideWorldBounds(true);
-    this.physics.add.collider(Princess, platforms);
+    this.physics.add.collider(Princess, Platforms);
     Princess.play('play');
+    Princess.setScale(2, 2);
     
  //Fire
  this.anims.create({
@@ -322,78 +358,89 @@ DK.play('play1')
   frameRate: 8,
   repeat: -1
 });
-    fire1 = this.physics.add.sprite(15, 305, 'fire1');
+    fire1 = this.physics.add.sprite(205, 305, 'fire1');
     fire1.setBounce(0.1);
     fire1.setCollideWorldBounds(true);
     this.physics.add.collider(fire1, platforms);
     fire1.play('play2');
+    fire1.setScale(2, 2);
     
-    //Barrell
-    Barrel = this.physics.add.sprite(0, 0, 'Barrel');
-    Barrel.setBounce(.1);
-    Barrel.setCollideWorldBounds(true);
-    this.physics.add.collider(Barrel, layer);
-
-
-  // Mario
-  //player = this.physics.add.sprite(5, 345, 'player');
-  //this.player = this.physics.add.sprite(5,210, 'player');
-  //Physics
-  //player.setBounce(0.2);
-  //player.setCollideWorldBounds(true);
-  // scale down
-  //player.setScale(0.025);
-  // player
-  //donk = this.add.sprite(160, 40, 'donk');
-  // scale down
-  //donk.setScale(0.1);
-
+  
 
   //  Player physics properties. Give the little guy a slight bounce.
 /*
   const debugGraphics = this.add.graphics().setAlpha(0.75);
-  layer.renderDebug(debugGraphics, {
+  Platforms.renderDebug(debugGraphics, {
     tileColor: null, // Color of non-colliding tiles
     collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
     //faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
   });
+
+  const debugGraphics1 = this.add.graphics().setAlpha(0.75);
+  Ladders.renderDebug(debugGraphics1, {
+    tileColor: null, // Color of non-colliding tiles
+    collidingTileColor: new Phaser.Display.Color(123, 194, 78, 255), // Color of colliding tiles
+    //faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+  });
 */
-  bg.setOrigin(0, 0);
+  //bg.setOrigin(0, 0);
 
-  /*
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
-});
-
-this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'player', frame: 4 } ],
-    frameRate: 20
-});
-
-this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-});
-*/
-
+	
+  //this.physics.add.collider(Ladders, mario);
+  this.physics.add.collider(mario, Enemy,hitBomb,null,this);
   cursors = this.input.keyboard.createCursorKeys();
 
-  this.physics.add.collider(mario, layer);
-  this.physics.add.collider(Princess, layer);
-  this.physics.add.collider(DK, layer);
-  this.physics.add.collider(Barrelling, layer);
-  this.physics.add.collider(fire1, layer);
+  this.physics.add.collider(mario, Platforms);
+  this.physics.add.collider(DK, Ladders);
+  this.physics.add.collider(Princess, Platforms);
+  this.physics.add.collider(DK, Platforms);
+ // this.physics.add.collider(Barrelling, Platforms);
+  this.physics.add.collider(fire1, Platforms);
 }
 
-// executed on every frame (60 times per second)
-function update() {
+function damageEnemy(enemy, bullet) {  
+  // only if both enemy and bullet are alive
+  if (enemy.active === true && bullet.active === true) {
+      // we remove the bullet right away
+      bullet.setActive(false);
+      bullet.setVisible(false);    
+      
+      // decrease the enemy hp with BULLET_DAMAGE
+      enemy.receiveDamage(BULLET_DAMAGE);
+  }
+}
 
+function hitBomb (mario, Enemy)
+{
+    this.physics.pause();
+
+    mario.setTint(0xff0000);
+
+    player.anims.play('hit');
+
+    gameOver = true;
+}
+
+
+
+// executed on every frame (60 times per second)
+function update(time, delta) {
+
+
+
+    if (time > this.nextEnemy)
+    {
+        var enemy = enemies.get();
+        if (enemy)
+        {
+            enemy.setActive(true);
+            enemy.setVisible(true);
+            enemy.startOnPath();
+  
+            this.nextEnemy = time + 2000;
+             
+    }
+  }
   if (gameOver) {
     return;
   }
@@ -406,19 +453,18 @@ function update() {
     mario.setVelocityX(75);
 
     mario.anims.play('right', true);
-  } else if (cursors.up.isDown) {
-    mario.setVelocityY(75);
-
-    mario.anims.play('right', true);
+  
   }else {
     mario.setVelocityX(0);
 
     mario.anims.play('turn');
   }
 
-  if (cursors.up.isDown && mario.body.touching.down) {
-    mario.setVelocityY(150);
-  }
+   // Jumping
+   if ((cursors.space.isDown || cursors.up.isDown) && mario.body.onFloor())
+   {
+       mario.body.setVelocityY(-200);
+   }
   // //check for active input
   // if (this.input.activePointer.isDown) {
 
